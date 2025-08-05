033c09b8d287f37258f1e7584aec372b358d01aa
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:56:49 -0700

    Create 866-RectangleOverlap

---
+++ 866-RectangleOverlap/866-RectangleOverlap.java
@@ -0,0 +1,7 @@
+// Last updated: 8/5/2025, 2:56:49 PM
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+       return (Math.min(rec1[2],rec2[2]) > Math.max(rec1[0],rec2[0]) &&
+        Math.min(rec1[3],rec2[3]) > Math.max(rec1[1],rec2[1]));
+    } 
+}
