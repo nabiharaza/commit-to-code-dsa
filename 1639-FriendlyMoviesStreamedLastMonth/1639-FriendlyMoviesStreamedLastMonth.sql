263a83e3a20517ce96eb51c41b6a049ceeb48002
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:53:49 -0700

    Create 1639-FriendlyMoviesStreamedLastMonth

---
+++ 1639-FriendlyMoviesStreamedLastMonth/1639-FriendlyMoviesStreamedLastMonth.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/5/2025, 2:53:49 PM
+SELECT 
+    DISTINCT c.title
+FROM 
+    Content c
+JOIN 
+    TVProgram p
+ON 
+    c.content_id = p.content_id
+WHERE 
+    c.Kids_content = 'Y'
+AND 
+    c.content_type = 'Movies'
+AND MONTH(p.program_date) = 6 AND YEAR(p.program_date) = 2020
