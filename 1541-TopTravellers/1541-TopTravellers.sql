8056640c57eecef71e906c122ccf8132c0972072
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:54:00 -0700

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/5/2025, 2:54:00 PM
+# Write your MySQL query statement below
+SELECT
+    name,
+    COALESCE(SUM(distance), 0) AS travelled_distance
+FROM
+    Users u
+    LEFT JOIN Rides r
+ON
+    r.user_id = u.id
+GROUP BY
+    u.id
+ORDER BY
+   travelled_distance DESC,
+   name;
