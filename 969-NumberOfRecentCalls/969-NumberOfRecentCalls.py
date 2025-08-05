644739168b0ec60d4f48aaa0a66c4132caf7854e
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:56:24 -0700

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.py
@@ -0,0 +1,15 @@
+# Last updated: 8/5/2025, 2:56:24 PM
+class RecentCounter:
+
+    def __init__(self):
+        self.slide_window = deque()
+
+    def ping(self, t: int) -> int:
+        # step 1). append the current call
+        self.slide_window.append(t)
+
+        # step 2). invalidate the outdated pings
+        while self.slide_window[0] < t - 3000:
+            self.slide_window.popleft()
+
+        return len(self.slide_window)
