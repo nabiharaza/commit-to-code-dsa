da8a4e56690aa7096e38b72cb8594a8f569194c5
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:53:54 -0700

    Create 1578-ApplesOranges

---
+++ 1578-ApplesOranges/1578-ApplesOranges.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/5/2025, 2:53:54 PM
+SELECT 
+    sale_date,
+    SUM(CASE WHEN fruit IN ('apples') THEN sold_num 
+             WHEN fruit IN ('oranges') THEN (sold_num)*-1 
+        END) AS diff
+FROM 
+    Sales
+GROUP BY 1
+ORDER BY 1
