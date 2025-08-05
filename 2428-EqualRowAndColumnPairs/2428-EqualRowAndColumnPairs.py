12d79da7d5593ed177c8e8e1a12dd61f1614d29b
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:52:26 -0700

    Create 2428-EqualRowAndColumnPairs

---
+++ 2428-EqualRowAndColumnPairs/2428-EqualRowAndColumnPairs.py
@@ -0,0 +1,15 @@
+# Last updated: 8/5/2025, 2:52:26 PM
+#Hash Map
+
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        count = 0
+        n = len(grid)
+
+        row_counter = collections.Counter(tuple(row) for row in grid)
+
+        for c in range(n):
+            col = [grid[i][c] for i in range(n)]
+            count += row_counter[tuple(col)]
+
+        return count
