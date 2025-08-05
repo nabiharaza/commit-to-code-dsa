6f11778b10e5759ebc026f427c4506e9f6b29433
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:53:53 -0700

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.py
@@ -0,0 +1,7 @@
+# Last updated: 8/5/2025, 2:53:53 PM
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        res = [nums[0]]
+        for i in range(1, len(nums)):
+            res.append(res[i-1] + nums[i])
+        return res
