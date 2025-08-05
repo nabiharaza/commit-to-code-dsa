f353a3200e75f50edc33e68239c75e1ebcc86df7
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:53:56 -0700

    Create 1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero

---
+++ 1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero/1576-ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
@@ -0,0 +1,30 @@
+# Last updated: 8/5/2025, 2:53:55 PM
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        edges = { (a,b) for a, b in connections}
+        neighbors = {city:[] for city in range(n)}
+        visited = set()
+        changes = 0
+
+        for a,b in connections:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
+
+        def dfs(city):
+            nonlocal changes, edges, neighbors, visited
+
+            for neighbor in neighbors[city]:
+
+                if neighbor in visited:
+                    continue
+                if (neighbor, city) not in edges:
+                    changes += 1
+                visited.add(neighbor)
+                dfs(neighbor)
+
+        visited.add(0)
+        dfs(0)
+        return changes
+
+# TC - O(N) 
+# SC - O(N)  
