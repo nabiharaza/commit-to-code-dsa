31b6e2dec06165a1bd8df359cc3c99d0c1979ef4
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:55:42 -0700

    Create 1135-CustomersWhoBoughtAllProducts

---
+++ 1135-CustomersWhoBoughtAllProducts/1135-CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/5/2025, 2:55:41 PM
+# Write your MySQL query statement below
+SELECT
+    customer_id
+FROM
+    Customer 
+GROUP BY
+    customer_id
+HAVING
+    COUNT(DISTINCT product_key) = 
+    (SELECT
+        COUNT(DISTINCT product_key)
+    FROM
+        Product);
+
