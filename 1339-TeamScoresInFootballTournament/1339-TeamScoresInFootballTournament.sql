84fc92903c2a8de6f86f31980bafab98470c7e52
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:54:30 -0700

    Create 1339-TeamScoresInFootballTournament

---
+++ 1339-TeamScoresInFootballTournament/1339-TeamScoresInFootballTournament.sql
@@ -0,0 +1,22 @@
+-- Last updated: 8/5/2025, 2:54:30 PM
+# Write your MySQL query statement below
+SELECT
+    team_id,
+    team_name,
+    SUM(CASE
+        WHEN team_id = host_team AND host_goals > guest_goals THEN 3
+        WHEN team_id = guest_team AND guest_goals > host_goals THEN 3
+        WHEN team_id = host_team AND host_goals = guest_goals THEN 1
+        WHEN team_id = guest_team AND host_goals = guest_goals THEN 1
+        ELSE 0
+    END) AS num_points
+FROM
+    Teams
+    LEFT JOIN Matches
+ON
+    team_id = host_team OR team_id = guest_team
+GROUP BY
+    team_id
+ORDER BY
+    num_points DESC,
+    team_id;
