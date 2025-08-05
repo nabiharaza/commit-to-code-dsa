a9e9f3e619045c0c66dd43f8a2ad5511fcbb145d
Nabiha Raza <nabiha_137@hotmail.com>
2025-08-05 14:56:19 -0700

    Create 990-VerifyingAnAlienDictionary

---
+++ 990-VerifyingAnAlienDictionary/990-VerifyingAnAlienDictionary.py
@@ -0,0 +1,18 @@
+# Last updated: 8/5/2025, 2:56:18 PM
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_index = {c: i for i, c in enumerate(order)}
+        
+        for i in range(len(words) - 1):
+            word1 = words[i]
+            word2 = words[i+1]
+            
+            for k in range(min(len(word1), len(word2))):
+                if word1[k] != word2[k]:
+                    if order_index[word1[k]] > order_index[word2[k]]:
+                        return False
+                    break
+            else:
+                if len(word1) > len(word2):
+                    return False
+        return True
